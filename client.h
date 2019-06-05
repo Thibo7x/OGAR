7fb4a5d9a57c0e974a8761d200530b12a311fac3
Thibo7x <thibaut@photonshuttle.space>
2019-06-05 08:21:30 +0200

    liste chainee etape 0

--- client.h
+++ client.h
@@ -51,6 +51,7 @@ typedef struct rencontre {
     unsigned int coordX;
     unsigned int coordY;
     unsigned char couleur[4];
+    rencontre *next;
 } rencontre;
 void moveBot(struct lws* wsi,unsigned int coordX,unsigned int coordY);
 
