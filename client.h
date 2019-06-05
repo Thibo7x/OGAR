102a7f6109a36aeb5219b8db30949be641895c92
Dromadaire <dromadaire@localhost.localdomain>
2019-06-05 15:49:59 +0200

    ajouté les stats des sheeps (R_ACTION , X/Y_VIEW)

--- client.h
+++ client.h
@@ -6,9 +6,9 @@
 //!!! AJOUTER SHEEP
 unsigned char CODES_ASCII[][8] = { {0x0, 0x72, 0x65, 0x64, 0x0, 0x0, 0x0, 0x0}, {0x0, 0x62, 0x6C, 0x75, 0x65, 0x0, 0x0, 0x0}, {0x0, 0x67, 0x72, 0x65, 0x65, 0x6E, 0X0, 0x0}, {0x0, 0x79, 0x65, 0x6C, 0x6C, 0x6F, 0x77, 0x0}, {0x0, 0x63, 0x79, 0x61, 0x6E, 0x0, 0x0, 0x0}, {0x0, 0x70, 0x75, 0x72, 0x70, 0x6C, 0x65, 0x0} };
 int CODES_ASCII_LENGTH[] = { 5, 6, 7, 8, 6, 8 };
-const unsigned long R_ACTION[] = { 50, 0, 300, 100, 300, 200 };
-const unsigned long X_VIEW[] = { 200, 2000, 800, 600, 600, 400 };
-const unsigned long Y_VIEW[] = { 200, 2000, 800, 600, 600, 1600 };
+const unsigned long R_ACTION[] = { 50, 0, 300, 100, 300, 200 , 0};
+const unsigned long X_VIEW[] = { 200, 2000, 800, 600, 600, 400 , 0};
+const unsigned long Y_VIEW[] = { 200, 2000, 800, 600, 600, 1600 , 0};
 char* couleur;
 static int callbackOgar(struct lws *wsi, enum lws_callback_reasons reason, void *user, void *in, size_t len);
 void idColor(char* couleur);
