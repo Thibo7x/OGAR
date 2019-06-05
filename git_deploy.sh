42f32bf933a034935db10087bebb44a3ae8fa4f6
userir <userir@jessie.esisar.fr>
2019-06-05 13:10:36 +0200

    enlever possibilité d'écrasement

--- git_deploy.sh
+++ git_deploy.sh
@@ -4,4 +4,3 @@ read -r -p 'Commit message: ' desc  # prompt user for commit message
 git add .                           # track all files
 git add -u                          # track deletes
 git commit -m "$desc"               # commit with message
-git push                            # push
