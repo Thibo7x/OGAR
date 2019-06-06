e5bd759449dc482555d1b39520ab9ded4a85c1d0
Dromadaire <dromadaire@localhost.localdomain>
2019-06-06 15:07:35 +0200

    bot_blue a été split dans Blue/

--- git_deploy.sh
+++ git_deploy.sh
@@ -4,3 +4,4 @@ read -r -p 'Commit message: ' desc  # prompt user for commit message
 git add .                           # track all files
 git add -u                          # track deletes
 git commit -m "$desc"               # commit with message
+git pull
