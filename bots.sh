89de920750c064bac328277ee5652a747e28e07d
userir <userir@jessie.esisar.fr>
2019-06-13 13:21:31 +0200

    bots.sh fais pop le nombre de bot JAUNE souhaité , 1 bot bleu et 1 spectate , aussi il demande désormais le PORT de connection et le 4ème octet de l'IP du serveur.

--- bots.sh
+++ bots.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-read -r -p 'Nombre de bots: ' max
+read -r -p 'Nombre de bots jaune : ' max_yellow
+read -r -p 'IP serveur 192.168.130.XXX : ' Filler
+read -r -p 'Port de connection du serveur (2006 ou 1443) : ' Port
 let "length = 0"
 
 
-while [ $length -lt $max ]; do
-	./sock -p 2005 -n yellow 192.168.130.150 &
+while [ $length -lt $max_yellow ]; do
+	./sock -p $Port -n yellow 192.168.130.$Filler &
 	let "length++"
 done
+
+	./sock -p $Port -n blue 192.168.130.$Filler &
+	./spectacle -s -p $Port -n blue 192.168.130.$Filler
