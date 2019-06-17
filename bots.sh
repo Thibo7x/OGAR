9f98803f4a1f5d6dba8a394468819e85fb03afa7
dromadaire <dromadaire@localhost.localdomain>
2019-06-17 10:25:09 +0200

    bots.sh update ,

--- bots.sh
+++ bots.sh
@@ -7,9 +7,9 @@ let "length = 0"
 
 
 while [ $length -lt $max_yellow ]; do
-	./sock -p $Port -n yellow 192.168.130.$Filler &
+	gone-terminal -e "./sock -p $Port -n yellow 192.168.130.$Filler &"
 	let "length++"
 done
 
-	./sock -p $Port -n blue 192.168.130.$Filler &
+	gnome-terminal -e "./sock -p $Port -n blue 192.168.130.$Filler &"
 	./spectacle -s -p $Port -n blue 192.168.130.$Filler
