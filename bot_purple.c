f828233ada1922519659b2706f9154c6866c3229
userir <userir@jessie.esisar.fr>
2019-06-17 17:32:14 +0200

    Les fonctions externes à intel blue sont OP

---
+++ bot_purple.c
@@ -0,0 +1,81 @@
+#include "entities.h"
+#include "mapFunctions.h"
+#include "chainedListFunctions.h"
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <stdlib.h>
+#include "bot_purple.h"
+#define MAP_SIZE_X 9000
+#define MAP_SIZE_Y 6000
+
+int distance(int coordX1, int coordY1, int coordX2, int coordY2)
+{
+	return sqrt((coordX2-coordX1)*(coordX2-coordX1)+(coordY2-coordY1)*(coordY2-coordY1));
+}
+
+//On genère un circuit de 4 points , (0,2800);(1100,2800);(1100,3200);(0,3200) sur un 9000x6000. isOk
+void split_purple(void)
+{
+  // 1er point
+  points[0][0] = 0.0 ; //x0
+  points[0][1] = (float)((int)(MAP_SIZE_Y/2)-200);  //y0
+  // 2ème point
+  points[1][0] = (float)((int)MAP_SIZE_X/10+200); //x1
+  points[1][1] = points[0][1]; //y1 = y0
+  // 3ème point
+  points[2][0] = points[1][0];  //x2 = x1
+  points[2][1] = (float)((int)MAP_SIZE_Y/2+200); //y2
+  // 4ème point
+  points[3][0] = 0.0; //x3
+  points[3][1] = points[2][1]; //y3 = y2
+  // Out Haut (Y <= 3000)
+  target_top.X = points[1][0]+300;
+  target_top.Y = points[0][1]-900;
+  // Out Bas (Y > 3000)
+  target_bottom.X = points[1][0]+300;
+  target_bottom.Y = points[2][1]+900;
+}
+
+// void test_split_purpel(void)
+// {
+//   for (int n = 0 ; n < 4 ; ++n )
+//     {
+//       for (int p = 0 ; p <2 ; ++p)
+//       {
+//         printf("Valeur dans le tableau[%d][%d] = %f\n",n,p,points[n][p]);
+//       }
+//     }
+//    printf("Target Top = (%f,%f) ; Target Bot = (%f,%f)\n",target_top.X,target_top.Y,target_bottom.X,target_bottom.Y);
+//  }
+
+//A_REMPLACER par dog->coord. quand on aura intel_purple , sinon , isOK
+int get_closest_point(void)
+{
+  int distance_table[4] = {10817,10817,10817,10817};
+  int min = 0;
+  for (int rank = 0 ; rank < 4 ; ++rank)
+    {
+      distance_table[rank] = distance(A_REMPLACER_X,A_REMPLACER_Y,(int)points[rank][0],(int)points[rank][1]);
+      if (distance_table[rank] <= distance_table[min])
+        min = rank;
+    }
+    return min;
+}
+//isOk
+// void test_get_closest_point(void)
+// {
+//   for(int rank = 0 ; rank < 4 ; ++rank)
+//     {
+//       A_REMPLACER_X = points[rank][0];
+//       A_REMPLACER_Y = points[rank][1];
+//       printf("Le rank le plus proche est %d.\n",get_closest_point());
+//     }
+// }
+
+int main(int argc,char* argv[])
+{
+  split_purple();
+
+  return 1;
+}
---
+++ bot_purple.h
@@ -0,0 +1,18 @@
+#ifndef BOTPURPLE
+#define BOTPURPLE
+
+#include "entities.h"
+#include "mapFunctions.h"
+#include "chainedListFunctions.h"
+
+float points[4][2];
+void test_split_purpel(void);
+void split_purpel(void);
+
+coordF target_top;
+coordF target_bottom;
+int A_REMPLACER_X;
+int A_REMPLACER_Y;
+//coord intel_purple(rencontre *voisins);
+
+#endif
