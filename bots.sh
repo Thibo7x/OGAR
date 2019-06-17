51c4c3e0aab6fa5ddd9dccbe0b3394e4b0d1fbe1
userir <userir@jessie.esisar.fr>
2019-06-17 14:43:03 +0200

    Bots.sh prend un compte le mode duel.

--- bots.sh
+++ bots.sh
@@ -1,15 +1,27 @@
 #!/bin/bash
-
-read -r -p 'Nombre de bots jaune : ' max_yellow
+read -r -p 'Multi (2) ou Solo (1) ? : ' mode
+read -r -p 'Nombre de bots jaune : ' yellow
+read -r -p 'Nombre de bots bleu : ' blue
+read -r -p 'Nombre de bots cyan : ' cyan
+read -r -p 'Nombre de bots violet : ' purple
 read -r -p 'IP serveur 192.168.130.XXX : ' Filler
 read -r -p 'Port de connection du serveur (2006 ou 1443) : ' Port
-let "length = 0"
 
 
-while [ $length -lt $max_yellow ]; do
-	./sock -p $Port -n yellow 192.168.130.$Filler &
-	let "length++"
+for color in {"yellow","blue","cyan","purple"}
+do
+	let "max = $color"
+	echo $max
+	let "length = 0"
+	while [ $length -lt $max ]; do
+		./sock -p $Port -n $color 192.168.130.$Filler &
+		let "length++"
+	done
 done
 
-	./sock -p $Port -n blue 192.168.130.$Filler &
+if [ $mode = 1 ]; then
 	./spectacle -s -p $Port -n blue 192.168.130.$Filler
+fi
+if [ $mode = 2 ]; then
+	./duo -s -p $Port -n blue 192.168.130.$Filler
+fi
