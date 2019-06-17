b646f53b68d4925aa11727203904bf99fedd8011
userir <userir@jessie.esisar.fr>
2019-06-17 15:05:43 +0200

    petit up de bots.sh , la connection au multi est compromis (fais pop que 1 vert)

--- bots.sh
+++ bots.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 read -r -p 'Multi (2) ou Solo (1) ? : ' mode
+if [ $mode = 2 ]; then
+	read -r -p 'Droite (1) ou Gauche (0) ? : ' side
+fi
 read -r -p 'Nombre de bots jaune : ' yellow
 read -r -p 'Nombre de bots bleu : ' blue
 read -r -p 'Nombre de bots cyan : ' cyan
@@ -11,10 +14,9 @@ read -r -p 'Port de connection du serveur (2006 ou 1443) : ' Port
 for color in {"yellow","blue","cyan","purple"}
 do
 	let "max = $color"
-	echo $max
 	let "length = 0"
 	while [ $length -lt $max ]; do
-		./sock -p $Port -n $color 192.168.130.$Filler &
+		./sock -p $Port -n $color -S $side 192.168.130.$Filler &
 		let "length++"
 	done
 done
@@ -22,6 +24,7 @@ done
 if [ $mode = 1 ]; then
 	./spectacle -s -p $Port -n blue 192.168.130.$Filler
 fi
+
 if [ $mode = 2 ]; then
 	./duo -s -p $Port -n blue 192.168.130.$Filler
 fi
